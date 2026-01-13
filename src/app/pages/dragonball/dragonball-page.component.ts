d6299b7998f99651a1430218d5257f4d245c8491
guiniazo <santiagoguiaz151@gmail.com>
2026-01-12 22:14:29 -0300

    hasta S5/L7

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,22 +1,21 @@
-import {Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive} from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 interface Character {
-    id: number;
-    name: string;
-    power: number;
+  id: number;
+  name: string;
+  power: number;
 }
 
 @Component({
-    templateUrl: './dragonball-page.component.html',
+  templateUrl: './dragonball-page.component.html',
 })
-
-export class DragonBallComponent{
-    characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001 },
-    {id: 2, name: 'Vegeta', power: 8000 },
-    {id: 3, name: 'Piccolo', power: 3000 }
-
-
-    ]);
+export class DragonBallComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
+  ]);
 }
+
