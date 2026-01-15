1273aeb4da2f4d6a75194c787ff40bb686002f2b
guiniazo <santiagoguiaz151@gmail.com>
2026-01-15 02:24:03 -0300

    s5 terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,23 @@ import path from 'path';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
 import { DragonBallComponent } from './pages/dragonball/dragonball-page.component';
+import { DragonBallSuperComponent } from './pages/dragonball-super/dragonball-super-page.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: CounterPageComponent,
-    },
-    {
-        path: 'hero',
-        component: HeroPageComponent,
-    },
-    {
-        path: 'dragonball',
-        component: DragonBallComponent,
-    }, 
+  {
+    path: '',
+    component: CounterPageComponent,
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent,
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonBallSuperComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonBallComponent,
+  },
 ];
--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -11,11 +11,31 @@ interface Character {
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonBallComponent {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Piccolo', power: 3000 },
-    { id: 4, name: 'Yamcha', power: 500 },
-  ]);
-}
+  name = signal(' ');
+  power = signal(0);
 
+  characters = signal<Character[]>([{ id: 1, name: 'Goku', power: 9001 }]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+    console.log('condiciones dadas. Agregando personaje...');
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+
+    console.log('personaje agrgado exitosamente!');
+    this.resetValues();
+  }
+
+  resetValues() {
+    this.name.set(' ');
+    this.power.set(0);
+  }
+}
